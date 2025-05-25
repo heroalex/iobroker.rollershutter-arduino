2fdc122cfa910f5090bd695999afb8d822423745
alex <alex@eyesqubes.com>
2025-05-25 15:24:39 +0200

    npx @iobroker/create-adapter

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'rollershutter-arduino adapter settings': {
+        'en': 'Adapter settings for rollershutter-arduino',
+        'de': 'Adaptereinstellungen für rollershutter-arduino',
+        'ru': 'Настройки адаптера для rollershutter-arduino',
+        'pt': 'Configurações do adaptador para rollershutter-arduino',
+        'nl': 'Adapterinstellingen voor rollershutter-arduino',
+        'fr': "Paramètres d'adaptateur pour rollershutter-arduino",
+        'it': "Impostazioni dell'adattatore per rollershutter-arduino",
+        'es': 'Ajustes del adaptador para rollershutter-arduino',
+        'pl': 'Ustawienia adaptera dla rollershutter-arduino',
+        'uk': 'Налаштування адаптера для rollershutter-arduino',
+        'zh-cn': 'rollershutter-arduino的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option1',
+        'ru': 'вариант1',
+        'pt': 'opção1',
+        'nl': 'Option1',
+        'fr': 'option1',
+        'it': 'opzione1',
+        'es': 'Opción1',
+        'pl': 'opcja 1',
+        'uk': 'Варіант1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option2',
+        'ru': 'вариант2',
+        'pt': 'opção2',
+        'nl': 'Optie2',
+        'fr': 'Option2',
+        'it': 'opzione2',
+        'es': 'opción 2',
+        'pl': 'opcja2',
+        'uk': 'Варіант2',
+        'zh-cn': '选项2'
+    }
+};

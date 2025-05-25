bd1d07f04c79ab67ad54c4f11cd8780c37d2350a
alex <alex@eyesqubes.com>
2025-05-25 16:10:08 +0200

    .

--- main.js
+++ main.js
@@ -1,170 +1,293 @@
 'use strict';
 
-/*
- * Created with @iobroker/create-adapter v2.6.5
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const { SerialPort } = require('serialport');
 
 class RollershutterArduino extends utils.Adapter {
 
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
     constructor(options) {
         super({
             ...options,
             name: 'rollershutter-arduino',
         });
+
+        this.serialPort = null;
+        this.reconnectTimer = null;
+        this.isConnecting = false;
+
         this.on('ready', this.onReady.bind(this));
         this.on('stateChange', this.onStateChange.bind(this));
-        // this.on('objectChange', this.onObjectChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
     }
 
-    /**
-     * Is called when databases are connected and adapter received configuration.
-     */
     async onReady() {
-        // Initialize your adapter here
-
         // Reset the connection indicator during startup
-        this.setState('info.connection', false, true);
+        await this.setState('info.connection', false, true);
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
+        // Validate configuration
+        if (!this.config.serialPath) {
+            this.log.error('Serial path not configured');
+            return;
+        }
 
-        /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
-        await this.setObjectNotExistsAsync('testVariable', {
+        if (!this.config.rollershutters || this.config.rollershutters.length === 0) {
+            this.log.warn('No rollershutters configured');
+            return;
+        }
+
+        // Create objects for all configured rollershutters
+        await this.createRollershutterObjects();
+
+        // Initialize serial connection
+        await this.initializeSerialConnection();
+
+        this.log.info('Adapter started successfully');
+    }
+
+    async createRollershutterObjects() {
+        this.log.info('Creating rollershutter objects...');
+
+        for (const shutter of this.config.rollershutters) {
+            const baseName = this.sanitizeName(shutter.name);
+
+            // Create channel for this rollershutter
+            await this.setObjectNotExistsAsync(baseName, {
+                type: 'channel',
+                common: {
+                    name: shutter.name,
+                    role: 'blind'
+                },
+                native: {
+                    id: shutter.id,
+                    openCommand: shutter.openCommand,
+                    closeCommand: shutter.closeCommand,
+                    stopCommand: shutter.stopCommand
+                }
+            });
+
+            // Create command states
+            await this.setObjectNotExistsAsync(`${baseName}.open`, {
+                type: 'state',
+                common: {
+                    name: `${shutter.name} Open`,
+                    type: 'boolean',
+                    role: 'button',
+                    read: false,
+                    write: true,
+                    def: false
+                },
+                native: {}
+            });
+
+            await this.setObjectNotExistsAsync(`${baseName}.close`, {
             type: 'state',
             common: {
-                name: 'testVariable',
+                    name: `${shutter.name} Close`,
+                    type: 'boolean',
+                    role: 'button',
+                    read: false,
+                    write: true,
+                    def: false
+                },
+                native: {}
+            });
+
+            await this.setObjectNotExistsAsync(`${baseName}.stop`, {
+                type: 'state',
+                common: {
+                    name: `${shutter.name} Stop`,
                 type: 'boolean',
-                role: 'indicator',
-                read: true,
+                    role: 'button',
+                    read: false,
                 write: true,
+                    def: false
             },
-            native: {},
-        });
+                native: {}
+            });
 
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
-
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
-
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
-
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
-
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+            // Subscribe to state changes for this rollershutter
+            this.subscribeStates(`${baseName}.*`);
+        }
     }
 
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     * @param {() => void} callback
-     */
+    sanitizeName(name) {
+        return name.replace(/[^a-zA-Z0-9_]/g, '_').replace(/^_+|_+$/g, '');
+    }
+
+    async initializeSerialConnection() {
+        if (this.isConnecting) {
+            return;
+        }
+
+        this.isConnecting = true;
+        this.log.info(`Initializing serial connection to ${this.config.serialPath}`);
+
+        try {
+            this.serialPort = new SerialPort({
+                path: this.config.serialPath,
+                baudRate: this.config.baudRate || 9600,
+                autoOpen: false
+            });
+
+            this.serialPort.on('open', () => {
+                this.log.info(`Serial port opened: ${this.config.serialPath}`);
+                this.setState('info.connection', true, true);
+                this.isConnecting = false;
+
+                // Clear any existing reconnect timer
+                if (this.reconnectTimer) {
+                    clearTimeout(this.reconnectTimer);
+                    this.reconnectTimer = null;
+                }
+            });
+
+            this.serialPort.on('error', (err) => {
+                this.log.error(`Serial port error: ${err.message}`);
+                this.setState('info.connection', false, true);
+                this.isConnecting = false;
+                this.scheduleReconnect();
+            });
+
+            this.serialPort.on('close', () => {
+                this.log.info('Serial port closed');
+                this.setState('info.connection', false, true);
+                this.isConnecting = false;
+                this.scheduleReconnect();
+            });
+
+            this.serialPort.on('data', (data) => {
+                this.handleIncomingData(data.toString().trim());
+            });
+
+            // Open the port
+            this.serialPort.open((err) => {
+                if (err) {
+                    this.log.error(`Error opening serial port: ${err.message}`);
+                    this.isConnecting = false;
+                    this.scheduleReconnect();
+                }
+            });
+
+        } catch (error) {
+            this.log.error(`Error creating serial port: ${error.message}`);
+            this.isConnecting = false;
+            this.scheduleReconnect();
+        }
+    }
+
+    scheduleReconnect() {
+        if (this.reconnectTimer) {
+            return; // Already scheduled
+        }
+
+        const interval = this.config.reconnectInterval || 5000;
+        this.log.info(`Scheduling reconnect in ${interval}ms`);
+
+        this.reconnectTimer = setTimeout(() => {
+            this.reconnectTimer = null;
+            this.initializeSerialConnection();
+        }, interval);
+    }
+
+    handleIncomingData(data) {
+        this.log.debug(`Received data: ${data}`);
+        // TODO: Parse feedback from Arduino when physical buttons are pressed
+        // This would be implemented based on the actual feedback format from Arduino
+    }
+
+    async sendCommand(command) {
+        if (!this.serialPort || !this.serialPort.isOpen) {
+            this.log.warn(`Cannot send command "${command}" - serial port not open`);
+            return false;
+        }
+
+        const message = command.endsWith('\n') ? command : command + '\n';
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                this.serialPort.write(message, (err) => {
+                    if (err) {
+                        this.log.error(`Error sending command "${command}": ${err.message}`);
+                        resolve(false);
+                    } else {
+                        this.log.info(`Command sent: ${command}`);
+                        resolve(true);
+                    }
+        });
+            }, this.config.openDelay || 100);
+        });
+    }
+
+    async onStateChange(id, state) {
+        if (state && !state.ack) {
+            const idParts = id.split('.');
+            const command = idParts[idParts.length - 1]; // 'open', 'close', or 'stop'
+            const shutterName = idParts.slice(2, -1).join('.'); // Everything between namespace and command
+
+            // Find the corresponding rollershutter configuration
+            const shutter = this.config.rollershutters.find(s =>
+                this.sanitizeName(s.name) === shutterName
+            );
+
+            if (!shutter) {
+                this.log.warn(`No configuration found for rollershutter: ${shutterName}`);
+                return;
+            }
+
+            let commandToSend = '';
+            switch (command) {
+                case 'open':
+                    commandToSend = shutter.openCommand;
+                    break;
+                case 'close':
+                    commandToSend = shutter.closeCommand;
+                    break;
+                case 'stop':
+                    commandToSend = shutter.stopCommand;
+                    break;
+                default:
+                    this.log.warn(`Unknown command: ${command}`);
+                    return;
+            }
+
+            this.log.info(`Sending ${command} command for ${shutter.name}: ${commandToSend}`);
+            const success = await this.sendCommand(commandToSend);
+
+            // Acknowledge the state
+            await this.setState(id, false, true);
+        }
+    }
+
     onUnload(callback) {
         try {
-            // Here you must clear all timeouts or intervals that may still be active
-            // clearTimeout(timeout1);
-            // clearTimeout(timeout2);
-            // ...
-            // clearInterval(interval1);
+            // Clear reconnect timer
+            if (this.reconnectTimer) {
+                clearTimeout(this.reconnectTimer);
+                this.reconnectTimer = null;
+            }
 
+            // Close serial port
+            if (this.serialPort && this.serialPort.isOpen) {
+                this.serialPort.close((err) => {
+                    if (err) {
+                        this.log.error(`Error closing serial port: ${err.message}`);
+                    } else {
+                        this.log.info('Serial port closed successfully');
+                    }
+                    callback();
+                });
+            } else {
             callback();
+            }
         } catch (e) {
+            this.log.error(`Error during unload: ${e.message}`);
             callback();
         }
     }
-
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  * @param {string} id
-    //  * @param {ioBroker.Object | null | undefined} obj
-    //  */
-    // onObjectChange(id, obj) {
-    //     if (obj) {
-    //         // The object was changed
-    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    //     } else {
-    //         // The object was deleted
-    //         this.log.info(`object ${id} deleted`);
-    //     }
-    // }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {string} id
-     * @param {ioBroker.State | null | undefined} state
-     */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
-        }
-    }
-
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    //     if (typeof obj === 'object' && obj.message) {
-    //         if (obj.command === 'send') {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info('send command');
-
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    //         }
-    //     }
-    // }
-
 }
 
 if (require.main !== module) {
-    // Export the constructor in compact mode
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
     module.exports = (options) => new RollershutterArduino(options);
 } else {
-    // otherwise start the instance directly
     new RollershutterArduino();
 }
